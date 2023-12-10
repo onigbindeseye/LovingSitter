9a79a1fc8fda193494efdd91d67b4cf60f9b888e
onigbindeseye <seyeonigbinde87@gmail.com>
2023-12-10 11:16:54 +0100

    Initial Setup of LovingSitter App

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { HelmetProvider } from "react-helmet-async";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { Route, Switch } from "react-router-dom";
+import Login from "./pages/Login/Login";
+import Home from "./pages/Homepage/Homepage";
+import SignUp from "./pages/SignUp/SignUp";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: "Open Sans",
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <HelmetProvider>
+      <ThemeProvider theme={theme}>
+        <div className="App">
+          <Switch>
+            <Route exact path="/signup" component={SignUp} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/" component={Home} />
+          </Switch>
+        </div>
+      </ThemeProvider>
+    </HelmetProvider>
   );
 }
 
